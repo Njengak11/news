ea31e214a79172c337830227fd239e1033fcd9ca
njenga1011 <njenga.k11@gmail.com>
2020-11-02 12:17:30 +0300

    added blog

--- lib/Views/home.dart
+++ lib/Views/home.dart
@@ -1,5 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news/helper/data.dart';
+import 'package:news/helper/news.dart';
+import 'package:news/model/category_model.dart';
+import 'package:news/model/article_model.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -7,18 +13,156 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+
+  List<CategoryModel> categories = new List<CategoryModel>();
+  List<ArticleModel> articles = new List<ArticleModel>();
+
+  bool _loading= true;
+
   @override
+  void initState() { 
+    super.initState();
+    categories = getCategories();
+    getNews();
+  }
+ getNews() async { 
+   News news = News();
+   await news.getNews();
+   articles = news.news;
+   setState(() {
+     _loading = false;
+   });
+ }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar( 
-        title: Row(children: [
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
           Text('GO',
-          style: Google),
-          Text('NEWS')
+          style: GoogleFonts.varelaRound(
+            color:Colors.red,
+            fontWeight: FontWeight.w700,
+          ),
+          ),
+          Text('NEWS',
+          style: GoogleFonts.oldStandardTt()
+          )
         ],),
         elevation: 0.0,
         centerTitle: true,
       ),
+      body: _loading  ? Center(
+        child: Container( 
+          child: CircularProgressIndicator(),
+        ),
+      ): SingleChildScrollView (
+        child:Container(
+          padding: EdgeInsets.symmetric(horizontal: 15),
+          child: Column(
+            children: [
+            //categories
+            Container( 
+              height: 70,
+              child: ListView.builder(
+                itemCount: categories.length,
+                shrinkWrap: true,
+                scrollDirection:Axis.horizontal,
+                itemBuilder: (context, index){
+                  return CategoryTile( 
+                    imageUrl: categories[index].imageUrl,
+                    categoryName: categories[index].categoryName,
+                  );
+                }
+                ),
+            ),
+
+            //blogs
+            Container(
+              padding: EdgeInsets.only(top: 15), 
+              child: ListView.builder(
+                itemCount: articles.length,
+                shrinkWrap: true,
+                physics: ClampingScrollPhysics(),
+                itemBuilder: (context, index){
+                  return BlogTile( 
+                    imageUrl: articles[index].urlToImage,
+                    title: articles[index].title,
+                    desc: articles[index].description,
+
+                  );
+                },
+                ),
+            )
+          ],),
+        ) ,
+      ),
+    );
+  }
+}
+
+class CategoryTile extends StatelessWidget {
+
+   final imageUrl, categoryName;
+  CategoryTile({this.imageUrl, this.categoryName});
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: (){},
+          child: Container(
+        margin: EdgeInsets.only(right:15),
+        child: Stack(children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: CachedNetworkImage(
+              imageUrl: imageUrl, width: 120,height: 60, fit: BoxFit.cover,)),
+          Container( 
+            alignment: Alignment.center,
+            width: 120,height: 60,
+            decoration: BoxDecoration( 
+              borderRadius: BorderRadius.circular(6),
+            color: Colors.black26,
+            ),
+            child: Text(categoryName,style: GoogleFonts.oldStandardTt(
+              color: Colors.white,
+              fontSize: 15,
+              fontWeight: FontWeight.w500
+            ),
+            ),
+          )
+        ],),
+      ),
+    );
+  }
+}
+
+class BlogTile extends StatelessWidget {
+
+  final String imageUrl, title, desc;
+  BlogTile({@required this.imageUrl, @required this.title, @required this.desc,});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(bottom:10),
+      child: Column(children: [
+        ClipRRect(
+          borderRadius: BorderRadius.circular(5),
+          child: Image.network(imageUrl)),
+          SizedBox(height: 10,),
+        Text(title,
+        style: GoogleFonts.oldStandardTt(
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+          color: Colors.black87
+        ),),
+        SizedBox(height: 10,),
+        Text(desc,
+         style: GoogleFonts.oldStandardTt(
+          color: Colors.black54
+        ),
+        )
+      ],),
     );
   }
 }
---
+++ lib/helper/data.dart
@@ -0,0 +1,55 @@
+import 'package:news/model/category_model.dart';
+
+List<CategoryModel>getCategories(){
+
+  List<CategoryModel> category = new List<CategoryModel>();
+  CategoryModel categoryModel = new CategoryModel();
+  categoryModel = new CategoryModel();
+
+  //1
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Business";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1486406146926-c627a92ad1ab?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80";
+  category.add(categoryModel);
+  
+  //2
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Entertainemnt";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1525869811964-53594bfcb4b0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+  category.add(categoryModel);
+
+  //3
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "General";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=751&q=80";
+  category.add(categoryModel);
+
+  //4
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Health";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1532938911079-1b06ac7ceec7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80";
+  category.add(categoryModel);
+
+  //5
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Science";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1576319155264-99536e0be1ee?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+
+  //6
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Sports";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1461896836934-ffe607ba8211?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+
+  //7
+  categoryModel = new CategoryModel();
+  categoryModel.categoryName = "Technology";
+  categoryModel.imageUrl = "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80";
+  category.add(categoryModel);
+  
+
+  
+
+  return category;
+}
